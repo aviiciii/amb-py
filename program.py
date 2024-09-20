9431c49a44ef9486e7c405a3c9dceae109112b41
aviiciii <laavesh1@gmail.com>
2024-09-20 11:11:35 +0530

    done dana done

--- program.py
+++ program.py
@@ -3,54 +3,50 @@ import pandas as pd
 import re
 import datetime
 
-amb_limit = 20000
+amb_limit = 10000
 
 def days_in_month(date):
     mm, yyyy = map(int, date.split('/'))
     return (datetime.date(yyyy, (mm)+1, 1) - datetime.date(yyyy, mm, 1)).days
 
 # read by default 1st sheet of an excel file
-df = pd.read_excel('file.xls')
+df = pd.read_excel('new.xls')
 
-# get the row 5 to 18
-info = df.iloc[3:19]
-information = []
-statement_date = None
+def def_statement_date(df):
+    # get the row 5 to 18
+    info = df.iloc[3:19]
+    information = []
+    statement_date = None
 
-for i in range(0, len(info)):
-    if type(info.iloc[i][4]) is str:
-        information.append(info.iloc[i][4])
+    for i in range(0, len(info)):
+        if type(info.iloc[i][4]) is str:
+            information.append(info.iloc[i][4])
 
-    if not statement_date:
-        i = info.iloc[i][0]
+        if not statement_date:
+            i = info.iloc[i][0]
 
-        if type(i)==str and i.startswith('Statement From'):
-            statement_date = i
+            if type(i)==str and i.startswith('Statement From'):
+                statement_date = i
 
-statement_date_from, statement_date_to = re.findall(r'\d{2}/\d{2}/\d{4}', statement_date)
-statement_month = re.findall(r'\d{2}/\d{4}', statement_date)[0]
-statement_date_from = int(statement_date_from[:2])
-statement_date_to = int(statement_date_to[:2])
-# print(statement_date_from, statement_date_to, statement_month)
-# print(information)
+    statement_date_from, statement_date_to = re.findall(r'\d{2}/\d{2}/\d{4}', statement_date)
+    statement_month = re.findall(r'\d{2}/\d{4}', statement_date)[0]
+    statement_date_from = int(statement_date_from[:2])
+    statement_date_to = int(statement_date_to[:2])
 
+    return statement_month, statement_date_from, statement_date_to
 
+statement_month, statement_date_from, statement_date_to = def_statement_date(df)
 
+days = days_in_month(statement_month)
 
-
-# remove first 21 rows
+# remove the summary and set the columns
 df = df.iloc[19:]
-# set head row
 df.columns = df.iloc[0]
-# remove head row
 df = df[2:]
-
-# print(df.columns)
-# print(df.head())
 
 closing_balances ={}
 
-# get the date of the statement
+# get the closing balance of each day
 for i in range(len(df)):
     date = df.iloc[i]['Date']
     closing_balance = df.iloc[i]['Closing Balance']
@@ -60,12 +56,7 @@ for i in range(len(df)):
         date= int(date[:2])
         closing_balances[date] = closing_balance
 
-
-
-# get no. of the days in the month of mm-yyyy
-days = days_in_month(statement_month)
-# print(days)
-
+# get the closing balances of all days and the total
 total = 0
 for i in range(1, statement_date_to):
     if i not in closing_balances:
@@ -73,27 +64,46 @@ for i in range(1, statement_date_to):
 
     total += closing_balances[i]
 
-# print(closing_balances)
 
-# check amb cleared
-amb = total/days
+total_amb = amb_limit * days
+current_amb = total / statement_date_to
 
-if amb > amb_limit:
-    print('AMC is cleared')
+# find deviance
+deviance = current_amb - amb_limit
+
+if total > total_amb:
+    print('\n', '--  AMB Cleared  --', '\n')
+    print(f'Statement Date: {statement_date_from}/{statement_month} to {statement_date_to}/{statement_month}')
+
+    print('Curent AMB (till statement date):', "{:.2f}".format(current_amb), "(+" +  "{:.2f}".format((deviance)) +")")
 else:
-    print('AMC is not cleared by', amb_limit-amb)
+    print('\n','--  AMB Not Cleared  --', '\n')
+    print(f'Statement Date: {statement_date_from}/{statement_month} to {statement_date_to}/{statement_month}')
+    # calculate stuff
+    if total < total_amb:
+        
+        # find lacking amount
+        lacking_amount = total_amb - total
+        lacking_amount_per_day = lacking_amount / statement_date_to
+
+        # get the amount to keep per day
+        days_left = days - statement_date_to
+        amount_to_keep_per_day = lacking_amount / days_left
 
 
-current_dayaverage = total/statement_date_to
-print('Current day average:', current_dayaverage)
 
-total_amb_limit = amb_limit * days
-remaining = total_amb_limit - total
-print('Remaining amount to clear AMC:', remaining)
-days_remaining = days - statement_date_to
-# how to add to clear
-if amb < amb_limit:
-    print('You need to keep', remaining/days_remaining, 'per day to clear AMC')
+    if deviance < 0:
+        print('Curent AMB (till statement date):', "{:.2f}".format(current_amb), "(" +  "{:.2f}".format((deviance)) +")")
+    else:
+        print('Curent AMB (till statement date):', "{:.2f}".format(current_amb), "(+" +  "{:.2f}".format((deviance)) +")")
+    print('You need to keep', "{:.2f}".format(amount_to_keep_per_day), 'per day to maintain AMB', "(" + str(days_left) + " days)")
+
+
+
+
+
+
+
 
 
         

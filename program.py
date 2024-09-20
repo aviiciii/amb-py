dc1aebfcfed498b1983fb9a597e1dc11b3f40826
aviiciii <laavesh1@gmail.com>
2024-09-20 10:32:55 +0530

    core working

--- program.py
+++ program.py
@@ -3,6 +3,8 @@ import pandas as pd
 import re
 import datetime
 
+amb_limit = 20000
+
 def days_in_month(date):
     mm, yyyy = map(int, date.split('/'))
     return (datetime.date(yyyy, (mm)+1, 1) - datetime.date(yyyy, mm, 1)).days
@@ -27,7 +29,9 @@ for i in range(0, len(info)):
 
 statement_date_from, statement_date_to = re.findall(r'\d{2}/\d{2}/\d{4}', statement_date)
 statement_month = re.findall(r'\d{2}/\d{4}', statement_date)[0]
-print(statement_date_from, statement_date_to, statement_month)
+statement_date_from = int(statement_date_from[:2])
+statement_date_to = int(statement_date_to[:2])
+# print(statement_date_from, statement_date_to, statement_month)
 # print(information)
 
 
@@ -42,13 +46,59 @@ df.columns = df.iloc[0]
 df = df[2:]
 
 # print(df.columns)
-print(df.head())
+# print(df.head())
+
+closing_balances ={}
+
+# get the date of the statement
+for i in range(len(df)):
+    date = df.iloc[i]['Date']
+    closing_balance = df.iloc[i]['Closing Balance']
+    if (type(date) is float) or (type(date) is str and date.startswith('*')):
+        break
+    if date != df.iloc[i+1][0]:
+        date= int(date[:2])
+        closing_balances[date] = closing_balance
 
 
 
 # get no. of the days in the month of mm-yyyy
 days = days_in_month(statement_month)
-print(days)
+# print(days)
+
+total = 0
+for i in range(1, statement_date_to):
+    if i not in closing_balances:
+        closing_balances[i] = closing_balances[i-1]
+
+    total += closing_balances[i]
+
+# print(closing_balances)
+
+# check amb cleared
+amb = total/days
+
+if amb > amb_limit:
+    print('AMC is cleared')
+else:
+    print('AMC is not cleared by', amb_limit-amb)
+
+
+current_dayaverage = total/statement_date_to
+print('Current day average:', current_dayaverage)
+
+total_amb_limit = amb_limit * days
+remaining = total_amb_limit - total
+print('Remaining amount to clear AMC:', remaining)
+days_remaining = days - statement_date_to
+# how to add to clear
+if amb < amb_limit:
+    print('You need to keep', remaining/days_remaining, 'per day to clear AMC')
+
+
+        
+
+
 
 
 
